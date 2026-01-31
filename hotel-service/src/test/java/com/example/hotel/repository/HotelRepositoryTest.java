a59d19fddaf4b4fa4deb9ebcbba07124273589b2
vishnu-r-1995 <vishnuvjd@gmail.com>
2026-01-31 19:25:55 +0530

    Add Hotel Repository testing class

---
+++ hotel-service/src/test/java/com/example/hotel/repository/HotelRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.hotel;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.example.hotel.*;
+
+@DataJpaTest
+class HotelRepositoryTest {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Test
+    void shouldSaveHotel() {
+        // given
+        Hotel hotel = new Hotel(
+                null,
+                "Gokulam",
+                "TVM",
+                "9876543210",
+                "gokulam@example.com"
+        );
+
+        // when
+        Hotel saved = hotelRepository.save(hotel);
+
+        // then
+        assertNotNull(saved.getHotelId(), "Hotel ID should be generated");
+        assertEquals("Gokulam", saved.getName());
+    }
+}
