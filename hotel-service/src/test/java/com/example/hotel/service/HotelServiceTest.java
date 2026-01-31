3ddeba7d226e255288981a62bb2d9558cc82fe60
vishnu-r-1995 <vishnuvjd@gmail.com>
2026-01-31 18:38:28 +0530

    Add unit test class for hotel-service

---
+++ hotel-service/src/test/java/com/example/hotel/service/HotelServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.hotel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.hotel.*;
+
+@ExtendWith(MockitoExtension.class)
+class HotelServiceTest {
+
+    @Mock
+    private HotelRepository hotelRepository;
+
+    @InjectMocks
+    private HotelService hotelService;
+
+    @Test
+    void shouldCreateHotel() {
+        Hotel hotel = new Hotel(null, "Turtle", "TVM", "1234567890", "h@gmail.com");
+        when(hotelRepository.save(any())).thenReturn(hotel);
+
+        Hotel saved = hotelService.create(hotel);
+
+        assertEquals("Turtle", saved.getName());
+        verify(hotelRepository).save(any());
+    }
+}
